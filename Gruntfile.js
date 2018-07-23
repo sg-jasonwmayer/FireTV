f1d869d2a5b1f2de0bdb3adf5280d0adadf5a2a9
Jason Mayer <jason@jasonmayer.com>
2018-07-23 00:53:57 -0400

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@
 // for performance reasons we're only matching one level down:
 // 'test/spec/{,*/}*.js'
 // use this if you want to recursively match all subfolders:
-// 'test/spec/**/*.js'
+// 'test/spec/**/*.jsx'
 
 module.exports = function(grunt) {
     var _host = '0.0.0.0';
